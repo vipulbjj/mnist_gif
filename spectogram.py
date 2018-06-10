c7247c4cfe500e126249b1d0f08472f4add23a76
Vipul Bajaj <vipulbjj@iitk.ac.in>
2018-06-10 11:06:15 +0530

    Update spectogram.py

--- spectogram.py
+++ spectogram.py
@@ -4,30 +4,7 @@ import matplotlib.pyplot as plt
 from scipy import signal
 import librosa
 import gc
-# def Diff(li1, li2):
-#     return (list(set(li1) - set(li2)))
 
-# directory1 = '/users/gpu/pandas/audio_vqa/data/OpenEnded_mscoco_train2014_questions/'
-# directory2 = '/users/gpu/pandas/audio_vqa/data/OpenEnded_mscoco_train2014_questions_Spectrograms1/'
-# lst1=[]
-# lst2=[]
-
-# for filename1 in os.listdir(directory1):
-#     lst1.append(str(filename1[:-4]))
-#     gc.collect()
-# for filename2 in os.listdir(directory2):
-#     lst2.append(str(filename2[:-4]))
-#     gc.collect()
-# count=0
-# lst1.sort()
-# lst2.sort()
-# print(lst1)
-# print(lst2)
-
-# lst3=Diff(lst1,lst2)
-# num_ques=len(lst3)
-# print(num_ques)
-# print(lst3)
 for i in range(1):
     # c=str(lst3[i])
     # # print(c)
